899aa5f838d08d984946e8c77d021ae98db092f4
eunchan96 <taegwon02@gmail.com>
2025-08-11 16:13:17 +0900

    step 08

--- src/main/kotlin/com/App.kt
+++ src/main/kotlin/com/App.kt
@@ -19,6 +19,8 @@ class App {
                 "등록" -> create()
                 "목록" -> list()
                 "삭제" -> delete(cmd)
+                "수정" -> modify(cmd)
+                else -> println("알 수 없는 명령입니다.")
             }
         }
     }
@@ -67,5 +69,34 @@ class App {
 
         wiseSayings.remove(wiseSaying)
         println("${id}번 명언이 삭제되었습니다.")
+    }
+
+    private fun modify(cmd: List<String>) {
+        if (cmd.size < 2) {
+            println("수정할 명언의 번호를 입력해주세요.")
+            return
+        }
+
+        val params = cmd[1].split("=")
+        if (params.size < 2 || params[0] != "id" || params[1].isBlank()) {
+            println("잘못된 명령입니다.")
+            return
+        }
+
+        val id = params[1].toIntOrNull()
+        val wiseSaying = wiseSayings.find { it.id == id } ?: run {
+            println("${id}번 명언은 존재하지 않습니다.")
+            return
+        }
+
+        println("명언(기존) : ${wiseSaying.content}")
+        print("명언 : ")
+        wiseSaying.content = readlnOrNull()!!.trim()
+
+        println("작가(기존) : ${wiseSaying.author}")
+        print("작가 : ")
+        wiseSaying.author = readlnOrNull()!!.trim()
+
+        println("${id}번 명언이 수정되었습니다.")
     }
 }
