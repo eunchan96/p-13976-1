6ecd9e9af2cd71702e79bb193c9e538a7f1fcbc0
eunchan96 <taegwon02@gmail.com>
2025-08-11 17:08:15 +0900

    step 11-2

--- src/main/kotlin/com/ll/wiseSaying/controller/WiseSayingController.kt
+++ src/main/kotlin/com/ll/wiseSaying/controller/WiseSayingController.kt
@@ -1,13 +1,9 @@
 package com.ll.wiseSaying.controller
 
-import com.ll.wiseSaying.entity.WiseSaying
 import com.ll.wiseSaying.service.WiseSayingService
 
 class WiseSayingController {
     private val wiseSayingService = WiseSayingService()
-
-    private var lastId = 0
-    private val wiseSayings = mutableListOf<WiseSaying>()
 
     fun create() {
         print("명언 : ")
@@ -15,22 +11,17 @@ class WiseSayingController {
         print("작가 : ")
         val author = readlnOrNull()!!.trim()
 
-        val wiseSaying = WiseSaying(++lastId, content, author)
-        wiseSayings.add(wiseSaying)
+        val wiseSaying = wiseSayingService.create(content, author)
 
-        println("${lastId}번 명언이 등록되었습니다.")
+        println("${wiseSaying.id}번 명언이 등록되었습니다.")
     }
 
     fun list() {
         println("번호 / 작가 / 명언")
         println("----------------------")
-        for (wiseSaying in findAll()) {
+        for (wiseSaying in wiseSayingService.findAll()) {
             println("${wiseSaying.id} / ${wiseSaying.author} / ${wiseSaying.content}")
         }
-    }
-
-    private fun findAll(): List<WiseSaying> {
-        return wiseSayings.reversed()
     }
 
     fun delete(cmd: List<String>) {
@@ -40,18 +31,17 @@ class WiseSayingController {
         }
 
         val params = cmd[1].split("=")
-        if (params.size < 2 || params[0] != "id" || params[1].isBlank()) {
+        if (params.size < 2 || params[0] != "id" || params[1].isBlank() || params[1].toIntOrNull() == null) {
             println("잘못된 명령입니다.")
             return
         }
 
-        val id = params[1].toIntOrNull()
-        val wiseSaying = wiseSayings.find { it.id == id } ?: run {
+        val id = params[1].toInt()
+        wiseSayingService.delete(id) ?: run {
             println("${id}번 명언은 존재하지 않습니다.")
             return
         }
 
-        wiseSayings.remove(wiseSaying)
         println("${id}번 명언이 삭제되었습니다.")
     }
 
@@ -62,24 +52,26 @@ class WiseSayingController {
         }
 
         val params = cmd[1].split("=")
-        if (params.size < 2 || params[0] != "id" || params[1].isBlank()) {
+        if (params.size < 2 || params[0] != "id" || params[1].isBlank() || params[1].toIntOrNull() == null) {
             println("잘못된 명령입니다.")
             return
         }
 
-        val id = params[1].toIntOrNull()
-        val wiseSaying = wiseSayings.find { it.id == id } ?: run {
+        val id = params[1].toInt()
+        val wiseSaying = wiseSayingService.findById(id) ?: run {
             println("${id}번 명언은 존재하지 않습니다.")
             return
         }
 
         println("명언(기존) : ${wiseSaying.content}")
         print("명언 : ")
-        wiseSaying.content = readlnOrNull()!!.trim()
+        val content = readlnOrNull()!!.trim()
 
         println("작가(기존) : ${wiseSaying.author}")
         print("작가 : ")
-        wiseSaying.author = readlnOrNull()!!.trim()
+        val author = readlnOrNull()!!.trim()
+
+        wiseSayingService.modify(id, content, author)
 
         println("${id}번 명언이 수정되었습니다.")
     }
--- src/main/kotlin/com/ll/wiseSaying/service/WiseSayingService.kt
+++ src/main/kotlin/com/ll/wiseSaying/service/WiseSayingService.kt
@@ -1,7 +1,46 @@
 package com.ll.wiseSaying.service
 
+import com.ll.wiseSaying.entity.WiseSaying
 import com.ll.wiseSaying.repository.WiseSayingRepository
 
 class WiseSayingService {
     private val wiseSayingRepository = WiseSayingRepository()
+
+    private var lastId = 0
+    private val wiseSayings = mutableListOf<WiseSaying>()
+
+    fun create(content: String, author: String): WiseSaying {
+        val wiseSaying = WiseSaying(++lastId, content, author)
+        wiseSayings.add(wiseSaying)
+
+        return wiseSaying
+    }
+
+    fun findAll(): List<WiseSaying> {
+        return wiseSayings.reversed()
+    }
+
+    fun findById(id: Int): WiseSaying? {
+        return wiseSayings.find { it.id == id }
+    }
+
+    private fun findIndexById(id: Int): Int {
+        return wiseSayings.indexOfFirst { it.id == id }
+    }
+
+    fun delete(id: Int): WiseSaying? {
+        val index = findIndexById(id)
+        return wiseSayings.removeAt(index)
+    }
+
+    fun modify(id: Int, content: String, author: String): WiseSaying? {
+        val index = findIndexById(id)
+        if (index == -1) return null
+
+        val wiseSaying = wiseSayings[index]
+        wiseSaying.content = content
+        wiseSaying.author = author
+
+        return wiseSaying
+    }
 }
